66cdba84aedda51ef21bd4845cf974481eacbb14
HungNguyen2318 <nguyenminhhung402@gmail.com>
2019-12-28 17:40:48 +0700

    Done

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyA7dWN-FFOZzg_lCrznpqjsMzPa_BQxbEI",
+    authDomain: "vue-note-5e32f.firebaseapp.com",
+    databaseURL: "https://vue-note-5e32f.firebaseio.com",
+    projectId: "vue-note-5e32f",
+    storageBucket: "vue-note-5e32f.appspot.com",
+    messagingSenderId: "483988259864",
+    appId: "1:483988259864:web:c9451b5e9934293f49af6d",
+    measurementId: "G-587RFMMGD7"
+}
